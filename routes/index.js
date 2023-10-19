f50b5c081b26600831f54ee73cff8ba8eb913564
Tova Greene <tovalgreene@gmail.com>
2023-10-19 17:18:47 -0400

    debugging

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+const router = Router();
+
 import userRoutes from './userRoutes';
+import thoughtRoutes from './thoughtRoutes';
+
+router.use('/api/thoughts', thoughtRoutes);
 router.use('/api/users', userRoutes);
 
-import thoughtRoutes from './thoughtRoutes';
-router.use('/api/thoughts', thoughtRoutes);
-
-export default Router;
+export default router;
